f621d31cc78b653b4c0e8cdbe5e16b128f3f03f0
VijaylaxmiRadder <vijaylaxmi.radder@volunteer.heartfulness.org>
2025-08-18 18:01:57 +0530

    Update output.tf

--- modules/alb/output.tf
+++ modules/alb/output.tf
@@ -1,3 +1,14 @@
-output "load_balancer_arn" {
-  value = aws_lb.load_balancer.arn
+output "alb_arn" {
+  description = "ARN of the Application Load Balancer"
+  value       = aws_lb.load_balancer.arn
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.load_balancer.dns_name
+}
+
+output "alb_id" {
+  description = "ID of the Application Load Balancer"
+  value       = aws_lb.load_balancer.id
 }
