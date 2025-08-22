c896685c84417225782f3aa4bdfc30ac365681a8
vijaylaxmi.radder <vijaylaxmi.radder@volunteer.heartfulness.org>
2025-08-22 16:42:21 +0530

    changed files

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -78,19 +78,6 @@ resource "aws_lb_listener" "https" {
   }
 }
 
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = 443
-  protocol          = "HTTPS"
-
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate_validation.cert_validation.certificate_arn
-
-  default_action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
-  }
-}
 
 /*resource "aws_lb_listener_rule" "https_rule1" {
   listener_arn = aws_lb_listener.https.arn
