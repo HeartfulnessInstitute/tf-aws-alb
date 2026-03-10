41f7be7e366ce50d926fdaeb0e6de817a78651b5
anitabai-m <anita.b@volunteer.heartfulness.org>
2026-03-10 15:35:12 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "hfn-alb-state-bucket"
+    bucket         = "hfn-alb-state-bucket-prod"
     key            = "production/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock-table"
