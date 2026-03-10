9cc9b6a27afd7f5f92c3c8960f6e88414ea679c9
anitabai-m <anita.b@volunteer.heartfulness.org>
2026-03-10 13:19:07 +0530

    Change Terraform state key to production

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
- terraform {
+terraform {
   backend "s3" {
     bucket         = "hfn-alb-state-bucket"
-    key            = "staging/terraform.tfstate"
+    key            = "production/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-lock-table"
     encrypt        = true
